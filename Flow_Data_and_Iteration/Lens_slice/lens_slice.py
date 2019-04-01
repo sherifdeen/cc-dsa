457efbb0648e167caebdd64fe87181e1b15764d5
Sherifdeen <sherifdeenlawal@icloud.com>
2019-04-01 17:08:03 -0500

    update lens slice project

--- Flow_Data_and_Iteration/Lens_slice/lens_slice.py
+++ Flow_Data_and_Iteration/Lens_slice/lens_slice.py
@@ -1,2 +1,34 @@
-Len's Slice
-You work at Len’s Slice, a new pizza joint in the neighborhood. You are going to use your knowledge of Python lists to organize some of your sales data.
+'''=============================================Len's Slice===============================================
+You work at Len’s Slice, a new pizza joint in the neighborhood. You are going to use your knowledge of Python 
+lists to organize some of your sales data.'''
+
+
+toppings = ['pepperoni', 'pineapple', 'cheese', 'sauage', 'olives', 'anchovies', 'mushrooms']
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_pizzas = len(toppings)
+
+print("We sell %d different kinds of pizza!" %(num_pizzas))
+
+pizzas = list(zip(prices, toppings))
+
+print(pizzas)
+
+pizzas.sort()
+
+cheapest_pizza = pizzas[0]
+priciest_pizza = pizzas[-1]
+
+
+three_cheapest = pizzas[:3]
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+
+print(num_two_dollar_slices)
+
+
+
+
+
